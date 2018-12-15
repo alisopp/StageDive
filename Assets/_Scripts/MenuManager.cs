a753ceb0abfc182d9bb4daf2966a57debdf00b3c
Alexander Isopp <alisopp@edu.aau.at>
2018-12-15 20:00:33 +0100

    added basic menu
    design is still needed

---
+++ Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Menu[] menus;
+    [SerializeField]
+    private int currentMenuIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void SwitchMenu(int menuIndex)
+    {
+        menus[currentMenuIndex].Disabe();
+        currentMenuIndex = menuIndex;
+        menus[currentMenuIndex].Enable();
+    }
+}
